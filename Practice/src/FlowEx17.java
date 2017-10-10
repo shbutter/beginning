e3eb3f20e6da8193600692c21146a772483e90f1
user <user@user-PC>
2017-10-10 23:59:03 +0900

    for

---
+++ Practice/src/FlowEx17.java
@@ -0,0 +1,40 @@
+
+public class FlowEx17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int j=5;j>=1;j--) {
+			for(int i=1;i<=j-1;i++) {
+				System.out.print(" ");
+			}
+			for(int i=5;i>=j;i--) {
+				System.out.print("*");
+			}
+			
+			for(int i=4;i>=j;i--) {
+				System.out.print("*");
+			}
+			
+		System.out.println();
+		}
+		
+		for(int j=5;j>=1;j--) {
+			for(int i=4;i>=j;i--) {
+				System.out.print(" ");
+			}
+			
+			for(int i=1;i<=j;i++) {
+				System.out.print("*");
+				
+			}
+			
+			for(int i=1;i<=j-1;i++) {
+				System.out.print("*");
+			}
+
+		System.out.println();
+		}
+		
+	}
+}
